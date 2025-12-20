a33361b17e3b67171f7d817ead31d02181119f3e
DMHCAIT <santoshapplication@dmhca.in>
2025-12-20 18:44:30 +0530

    Center logo in header on mobile, keep left-aligned on desktop

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,9 +26,9 @@ export default function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-[#FAF9F7]/95 backdrop-blur-md border-b border-[#C8A96A]/20 shadow-sm">
       <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between lg:justify-between items-center h-20 relative">
+        <div className="flex justify-between items-center h-20">
           {/* Logo - Centered on mobile, left on desktop */}
-          <Link href="/" className="flex items-center py-2 absolute left-1/2 -translate-x-1/2 lg:static lg:translate-x-0">
+          <Link href="/" className="absolute left-1/2 -translate-x-1/2 lg:static lg:translate-x-0 flex items-center py-2">
             <div className="relative h-12 w-auto aspect-[3/1] lg:h-16">
               <Image
                 src="/logo.jpg"
@@ -40,9 +40,6 @@ export default function Header() {
               />
             </div>
           </Link>
-
-          {/* Spacer for mobile to maintain layout */}
-          <div className="lg:hidden w-20"></div>
 
           {/* Desktop Navigation */}
           <div className="hidden lg:flex items-center space-x-8">
